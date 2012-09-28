226d9885f874b5c02c1863a2433c8cf8697b5c0a
Nilu <nlhpandey@gmail.com>
2012-09-28 19:15:15 -0400

    first commit and g scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pgtester::Application.config.secret_token = '0581fe1e7727627cf80dd443e5eea3525efe2df7f534558b21f5c390b7f75f6e30470c488bfd688515ab90d937ecbe6f6e571eae3a1cccc818ad5b5dff78ce6b'
